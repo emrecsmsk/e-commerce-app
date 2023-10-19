644a619b7733333f87618267b94d720aba34e2ec
Emre Can Şimşek <emrecan.midyat@gmail.com>
2023-10-19 18:48:44 +0300

    set navbar

--- src/navigation/Navigation.js
+++ src/navigation/Navigation.js
@@ -1,9 +1,30 @@
 import React from 'react'
 import "./Navigation.css"
+import { FiHeart } from "react-icons/fi"
+import { AiOutlineShoppingCart, AiOutlineUserAdd } from "react-icons/ai"
 
 const Navigation = () => {
     return (
-        <div>Navigation</div>
+        <nav>
+            <div className="nav-container">
+                <div className="nav"></div>
+                <input
+                    type="text"
+                    className="search-input"
+                    placeholder="Enter your search shoes." />
+            </div>
+            <div className="profile-container">
+                <a href="#">
+                    <FiHeart className="nav-icons" />
+                </a>
+                <a href="#">
+                    <AiOutlineShoppingCart className="nav-icons" />
+                </a>
+                <a href="#">
+                    <AiOutlineUserAdd className="nav-icons" />
+                </a>
+            </div>
+        </nav>
     )
 }
 
